c376efe56725970db890b91d28d6aa6d885176d9
Sagar Dagdu <shags032@gmail.com>
2021-06-02 20:34:06 +0530

    Add initial UI for weather view

--- WeatherSwiftUI/ContentView.swift
+++ WeatherSwiftUI/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            VStack(spacing: 8) {
+                Text("Pune")
+                    .foregroundColor(.white)
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .padding()
+                Image(systemName: "cloud.sun.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180)
+                Text("35°")
+                    .font(.system(size: 64, weight: .regular))
+                    .foregroundColor(.white)
+                HStack() {
+                    VStack(spacing :6) {
+                        Text("Mon")
+                            .foregroundColor(.white)
+                            .bold()
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        Text("31°")
+                            .font(.system(size: 22, weight: .regular))
+                            .foregroundColor(.white)
+                    }
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
 
