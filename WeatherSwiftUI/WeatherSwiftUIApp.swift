ca18206d08322049909fb08f557611cd36d70ea2
Sagar Dagdu <shags032@gmail.com>
2021-06-02 20:32:21 +0530

    Initial code

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Sagar Dagdu on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
